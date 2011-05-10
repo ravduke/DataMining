94b35ab81feea797c5b750adca48ce11ae3e6cb3
Tomasz Pyszka <tomasz.pyszka@gmail.com>
2011-05-10 19:15:55 +0200

    [51] javadoc added.

--- weka-communication/src/main/java/pl/edu/agh/ftj/datamining/gui/communication/WekaService.java
+++ weka-communication/src/main/java/pl/edu/agh/ftj/datamining/gui/communication/WekaService.java
@@ -1,17 +1,32 @@
 package pl.edu.agh.ftj.datamining.gui.communication;
 
 import java.util.List;
-
 import pl.edu.agh.ftj.datamining.gui.communication.impl.WekaCommunicationFactory.Type;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
+/*
+ * Serwis odpowiedzialny za komunikacje z klientem Weki.
+ * @author Tomasz Pyszka
+ */
 @RemoteServiceRelativePath("wekaservice")
 public interface WekaService extends RemoteService {
 
+	/*
+	 * Metoda odpowiedzialna za utworzenie odpowiedniego klienta.
+	 * @param typ komunikacji REST lub AXIS {@link Type}
+	 */
 	public void createCommunication(Type type);
 	
+	/*
+	 * Metoda odpowiedzialna za popbranie z klienta Weki dostepnych algorytmow.
+	 * @return zwraca dostepne algorytmy
+	 */
 	public List<String> getAlgorithms();
 	
+	/*
+	 * Metoda odpowiedzialna za uruchomienie za pomoca klienta algorytmu Weki.
+	 * @return Zwraca dane z algorytmu {@link WekaAnswer}.
+	 */
 	public WekaAnswer runAlgorithm(Integer algorithmType, String location, String id, String table);
 }
--- weka-communication/src/main/java/pl/edu/agh/ftj/datamining/gui/communication/impl/Utils.java
+++ weka-communication/src/main/java/pl/edu/agh/ftj/datamining/gui/communication/impl/Utils.java
@@ -3,8 +3,16 @@ package pl.edu.agh.ftj.datamining.gui.communication.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+ * W tej klasie znajduja sie pomocnicze metody.
+ */
 public class Utils {
 
+	/*
+	 * Metoda przetwarzajaca odpowiedz z Weka serwisu do postaci listy algorytmow.
+	 * @param odpowiedz z serwisu Weki
+	 * @return lista dostepnych algorytmow
+	 */
 	public static List<String> getAlgorithms(String param) {
 		List<String> result = new ArrayList<String>();
 		String[] tmp = param.split("<return>");
