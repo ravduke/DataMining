d792c019e8497cebb6a0e66875871625a639ce4f
Rafal Jablonowski <ravduke@gmail.com>
2011-05-12 14:26:55 +0200

    [74] Fixed compiling error. [45] New resources

--- GWTGUI/src/main/java/pl/edu/agh/ftj/datamining/client/GWTServiceGUI.java
+++ GWTGUI/src/main/java/pl/edu/agh/ftj/datamining/client/GWTServiceGUI.java
@@ -1,16 +1,17 @@
  package pl.edu.agh.ftj.datamining.client;
 
-
-import com.extjs.gxt.ui.client.Style.Orientation;
+ import com.extjs.gxt.samples.resources.client.Resources;
+ import com.extjs.gxt.samples.resources.client.TestData;
+import com.extjs.gxt.samples.resources.client.model.DBTable;
+ import com.extjs.gxt.samples.resources.client.model.Folder;
+import com.extjs.gxt.samples.resources.client.model.Music;
+ import com.extjs.gxt.ui.client.Style.Orientation;
  import com.extjs.gxt.ui.client.widget.layout.FlowData;
  import com.extjs.gxt.ui.client.widget.menu.MenuBar;
  import com.extjs.gxt.ui.client.widget.menu.MenuBarItem;
  import com.extjs.gxt.ui.client.widget.ContentPanel;
  import com.extjs.gxt.ui.client.widget.button.Button;
- import com.extjs.gxt.ui.client.widget.button.SplitButton;
- import com.extjs.gxt.ui.client.widget.button.ToggleButton;
- import com.extjs.gxt.ui.client.widget.menu.CheckMenuItem;
- import com.extjs.gxt.ui.client.widget.menu.DateMenu;
+
  import com.extjs.gxt.ui.client.widget.menu.Menu;
  import com.extjs.gxt.ui.client.widget.menu.MenuItem;
  import com.extjs.gxt.ui.client.widget.menu.SeparatorMenuItem;
@@ -18,10 +19,12 @@ import com.extjs.gxt.ui.client.Style.Orientation;
  import com.extjs.gxt.ui.client.widget.toolbar.ToolBar;
  import com.extjs.gxt.ui.client.Style.Scroll;
  import com.extjs.gxt.ui.client.data.BaseModel;
+ import com.extjs.gxt.ui.client.data.ModelData;
  import com.extjs.gxt.ui.client.event.ButtonEvent;
  import com.extjs.gxt.ui.client.event.ComponentEvent;
  import com.extjs.gxt.ui.client.event.Events;
  import com.extjs.gxt.ui.client.event.Listener;
+import com.extjs.gxt.ui.client.store.TreeStore;
  import com.extjs.gxt.ui.client.util.Margins;
  import com.extjs.gxt.ui.client.widget.LayoutContainer;
  import com.extjs.gxt.ui.client.widget.TabItem;
@@ -35,8 +38,13 @@ import com.extjs.gxt.ui.client.widget.form.TextField;
 
 import com.extjs.gxt.ui.client.widget.layout.RowData;
 import com.extjs.gxt.ui.client.widget.layout.RowLayout;
+import com.extjs.gxt.ui.client.widget.treepanel.TreePanel;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
  import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 
 
 
@@ -46,10 +54,11 @@ import com.google.gwt.user.client.Window;
  */
 public class GWTServiceGUI extends LayoutContainer {
 
+ Folder root;
  public  GWTServiceGUI() {
-
+    root = new Folder();
  }
-    /**
+     /**
      * Tworzy interfejs uzytkownika
      * @param parent Element docelowy
      * @param index Miejsce wstawienia
@@ -65,9 +74,9 @@ public class GWTServiceGUI extends LayoutContainer {
      ContentPanel panel = new ContentPanel();
      panel.setHeading("DataMining Application");
      panel.setWidth("99%");
-     panel.setHeight(530);
+     panel.setHeight(580);
 
-     tabPanel.setHeight(529);
+     tabPanel.setHeight(579);
 
      panel.setTopComponent(bar);
      panel.add(toolBar);
@@ -85,72 +94,38 @@ public class GWTServiceGUI extends LayoutContainer {
 
      Menu menu = new Menu();
 
-     MenuItem item1 = new MenuItem("New");
-     menu.add(item1);
-
-     MenuItem item2 = new MenuItem("Open File");
-     menu.add(item2);  
-
-     Menu sub = new Menu();
-     sub.add(new MenuItem("readme.txt"));
-     sub.add(new MenuItem("helloworld.txt"));
-     item2.setSubMenu(sub);
-
-     MenuItem item0 = new MenuItem("Exit");
-     menu.add(item0);
-
+     MenuItem itemResearch = new MenuItem("New Research");
+     menu.add(itemResearch);
+     menu.add(new SeparatorMenuItem());
+     MenuItem itemConnect = new MenuItem("Connect");
+     menu.add(itemConnect);
+     MenuItem itemRefresh = new MenuItem("Refresh");
+     menu.add(itemRefresh);
+     MenuItem itemDisconnect = new MenuItem("Disconnect");
+     menu.add(itemDisconnect);
+     menu.add(new SeparatorMenuItem());
+     MenuItem itemClear = new MenuItem("Clear Results");
+     menu.add(itemClear);
+     MenuItem itemGraphs = new MenuItem("Export Graphs");
+     menu.add(itemGraphs);
+     menu.add(new SeparatorMenuItem());
+     MenuItem  itemExit = new MenuItem("Exit");
+     menu.add( itemExit);
+     
      MenuBar bar = new MenuBar();
      bar.setBorders(true);
      bar.setStyleAttribute("borderTop", "none");
      bar.add(new MenuBarItem("File", menu));
 
-     Menu sub2 = new Menu();
-     sub2.add(new MenuItem("Cut"));
-     sub2.add(new MenuItem("Copy"));
-
-     MenuBarItem edit = new MenuBarItem("Options", sub2);
-     bar.add(edit);
-
-     sub = new Menu();
-     sub.add(new MenuItem("Search"));
-     sub.add(new MenuItem("File"));
-     sub.add(new MenuItem("Java"));
-
-     MenuBarItem item3 = new MenuBarItem("Help", sub);
+     menu = new Menu();
+     menu.add(new MenuItem("User Interface"));
+     menu.add(new MenuItem("Documentation"));
+     MenuBarItem item3 = new MenuBarItem("Help", menu);
      bar.add(item3);
 
      menu = new Menu();
-
      menu.add(new MenuItem("Information"));
      menu.add(new MenuItem("Contact"));
-     CheckMenuItem menuItem = new CheckMenuItem("Check Box");
-     menuItem.setChecked(true);
-     menu.add(menuItem);
-
-     menuItem = new CheckMenuItem("Check Box 2");
-     menu.add(menuItem);
-
-
-     menu.add(new SeparatorMenuItem());
-
-     MenuItem radios = new MenuItem("Radio Options");
-     menu.add(radios);
-
-     Menu radioMenu = new Menu();
-     CheckMenuItem r = new CheckMenuItem("Blue Theme");
-     r.setGroup("radios");
-     r.setChecked(true);
-     radioMenu.add(r);
-     r = new CheckMenuItem("Gray Theme");
-     r.setGroup("radios");
-     radioMenu.add(r);
-     radios.setSubMenu(radioMenu);
-
-     MenuItem date = new MenuItem("Choose a Date");
-     //date.setIcon(Resources.ICONS.calendar());
-     menu.add(date);
-
-     date.setSubMenu(new DateMenu());
 
      MenuBarItem item4 = new MenuBarItem("About", menu);
      bar.add(item4);
@@ -162,102 +137,49 @@ public class GWTServiceGUI extends LayoutContainer {
      */
   private ToolBar  createToolBar() {
 
-       ToolBar toolBar = new ToolBar();
-
-     Button item1 = new Button("Button w/ Menu");
-    // item1.setIcon(Resources.ICONS.menu_show());
-
-   /*  List<String> list = new ArrayList<String>();
-     //final ListStore<Stock> store = new ListStore<Stock>();
-
-     store.add(list);
-
-     final ComboBox<Stock> combo = new ComboBox<Stock>();
-     combo.setFieldLabel("Company");
-     combo.setDisplayField("name");
-     combo.setName("name");
-     combo.setValueField("symbol");
-     combo.setForceSelection(true);
-     combo.setStore(store);
-     combo.setTriggerAction(TriggerAction.ALL);
-
-     AdapterMenuItem adapter = new AdapterMenuItem(combo);
-     adapter.setManageFocus(true);
-     adapter.setCanActivate(true);*/
-
-     Menu menu = new Menu();
-    // menu.add(adapter);
-
-     CheckMenuItem menuItem = new CheckMenuItem("Check Box");
-     menuItem.setChecked(true);
-     menu.add(menuItem);
-
-     menuItem = new CheckMenuItem("Check Box 2");
-     menu.add(menuItem);
-     item1.setMenu(menu);
-
-     menu.add(new SeparatorMenuItem());
-
-     MenuItem radios = new MenuItem("Radio Options");
-     menu.add(radios);
-
-     Menu radioMenu = new Menu();
-     CheckMenuItem r = new CheckMenuItem("Blue Theme");
-     r.setGroup("radios");
-     r.setChecked(true);
-     radioMenu.add(r);
-     r = new CheckMenuItem("Gray Theme");
-     r.setGroup("radios");
-     radioMenu.add(r);
-     radios.setSubMenu(radioMenu);
-
-     MenuItem date = new MenuItem("Choose a Date");
-     //date.setIcon(Resources.ICONS.calendar());
-     menu.add(date);
-
-     date.setSubMenu(new DateMenu());
-
-     toolBar.add(item1);
+     ToolBar toolBar = new ToolBar();
 
      toolBar.add(new SeparatorToolItem());
-
-     SplitButton splitItem = new SplitButton("Split Button");
-    // splitItem.setIcon(Resources.ICONS.list_items());
-
-     menu = new Menu();
-     menu.add(new MenuItem("<b>Bold</b>"));
-     menu.add(new MenuItem("<i>Italic</i>"));
-     menu.add(new MenuItem("<u>Underline</u>"));
-     splitItem.setMenu(menu);
-
-     toolBar.add(splitItem);
+     Button newResearchButton = new Button("New Research");
+     toolBar.add(newResearchButton);
 
      toolBar.add(new SeparatorToolItem());
+     Button connectButton = new Button("Connect");
+     connectButton.setIcon(Resources.ICONS.menu_show());
+     final AsyncCallback<Folder> connectCallback = new AsyncCallback<Folder>() {
+        public void onSuccess(Folder result) {
+            root = result;
+        }
+        public void onFailure(Throwable caught) {
+            // lblServerReply.setText("Communication failed");
+        }
+     };
+        connectButton.addListener(Events.Select,
+            new Listener<ButtonEvent>(){
+            public void handleEvent(ButtonEvent e) {
+                getService().getDatabases(connectCallback);
+            }
+        });
 
-     ToggleButton toggle = new ToggleButton("Toggle");
-     toggle.toggle(true);
-     toolBar.add(toggle);
+     toolBar.add(connectButton);
 
      toolBar.add(new SeparatorToolItem());
-
-     Button scrollerButton = new Button("Scrolling Menu");
-
-     Menu scrollMenu = new Menu();
-     scrollMenu.setMaxHeight(200);
-     for (int i = 0; i < 40; i++) {
-       scrollMenu.add(new MenuItem("Item " + i));
-     }
-
-     scrollerButton.setMenu(scrollMenu);
-
-     toolBar.add(scrollerButton);
+     Button refreshButton = new Button("Refresh");
+     toolBar.add(refreshButton);
 
      toolBar.add(new SeparatorToolItem());
+     Button disconnectButton = new Button("Disconnect");
+     toolBar.add(disconnectButton);    
 
-    // toolBar.add(new FillToolItem());
-    // ThemeSelector selector = new ThemeSelector();
-    // toolBar.add(selector);
+     toolBar.add(new SeparatorToolItem());
+     Button clearButton = new Button("Clear Results");
+     toolBar.add(clearButton);
 
+     toolBar.add(new SeparatorToolItem());
+     Button exportGraphsButton = new Button("Export Graphs");
+     toolBar.add(exportGraphsButton);
+
+     toolBar.add(new SeparatorToolItem());
       return toolBar;
   }
 
@@ -270,14 +192,14 @@ public class GWTServiceGUI extends LayoutContainer {
      panel.setPlain(true);
      panel.setBorders(false);
     
-     TabItem connect = new TabItem("Connect");
+     TabItem connect = new TabItem("Select Data");
 
      ContentPanel simple = new ContentPanel();
      simple.setHeaderVisible(false);
      simple.setFrame(true);
      simple.setWidth("100%");
      simple.setHeight(479);
-     simple.setLayout(new RowLayout(Orientation.VERTICAL));
+     simple.setLayout(new RowLayout(Orientation.HORIZONTAL));
      //simple.setLayout(new BorderLayout());
      
      //FormData formData = new FormData("-500");
@@ -285,7 +207,7 @@ public class GWTServiceGUI extends LayoutContainer {
     // firstName.setFieldLabel("Name");
      //simple.add(firstName);//, formData);
 
-/*      COMBOBOX DATABASE TYPE        */
+/*      COMBOBOX DATABASE TYPE       
      SimpleComboBox combo = new SimpleComboBox<String>();
      combo.setEmptyText("Select database...");  
      //combo.setLabelSeparator("Database type:");
@@ -296,8 +218,8 @@ public class GWTServiceGUI extends LayoutContainer {
      combo.setTypeAhead(true);
      combo.setTriggerAction(TriggerAction.ALL);
      combo.setEditable(false);
-
-/*      COMBOBOX DATABASE NAME        */
+ */
+/*      COMBOBOX DATABASE NAME        
      final SimpleComboBox combo2 = new SimpleComboBox<String>();
      combo2.setEmptyText("Select database name...");
      //combo.setLabelSeparator("Database type:");
@@ -310,8 +232,8 @@ public class GWTServiceGUI extends LayoutContainer {
      combo2.setTriggerAction(TriggerAction.ALL);
      combo2.setEditable(false);
      combo2.setEnabled(false);
-
-   /*      COMBOBOX TABLE NAME        */
+*/
+   /*      COMBOBOX TABLE NAME        
 
     final SimpleComboBox<String> combo3 = new SimpleComboBox<String>();
      combo3.setEmptyText("Select table...");
@@ -325,6 +247,8 @@ public class GWTServiceGUI extends LayoutContainer {
      combo3.setTriggerAction(TriggerAction.ALL);
      combo3.setEditable(false);
      combo3.setEnabled(false);
+        */
+
    /*  ComboBox combo3 = new ComboBox();
      combo3.setEmptyText("Select table...");
      combo3.setLabelSeparator("Database type:");
@@ -345,10 +269,21 @@ public class GWTServiceGUI extends LayoutContainer {
      FormPanel p1 = new FormPanel();
      p1.setHeaderVisible(false);
      p1.setWidth("100%");
-     p1.setHeight(50);
-     p1.setLayout(new RowLayout(Orientation.HORIZONTAL));
-     
-     FormPanel p2 = new FormPanel();
+     p1.setHeight(470);
+     p1.setLayout(new RowLayout(Orientation.VERTICAL));
+
+    Folder model = root;
+
+    TreeStore<ModelData> store = new TreeStore<ModelData>();
+    store.add(model.getChildren(), true); 
+
+    final TreePanel<ModelData> tree = new TreePanel<ModelData>(store);
+
+    tree.setWidth(400);
+    tree.setDisplayProperty("name");
+    tree.getStyle().setLeafIcon(Resources.ICONS.music());
+    
+ /*    FormPanel p2 = new FormPanel();
      p2.setHeaderVisible(false);
      p2.setWidth("100%");
      p2.setHeight(50);
@@ -360,7 +295,8 @@ public class GWTServiceGUI extends LayoutContainer {
      p3.setWidth("100%");
      p3.setHeight(50);
      p3.setLayout(new RowLayout(Orientation.HORIZONTAL));
-
+     */
+/*
      TextField<String> database_type = new TextField<String>();
      database_type.setEnabled(false);
      database_type.setValue("Database type:");
@@ -379,10 +315,10 @@ public class GWTServiceGUI extends LayoutContainer {
      TextField<String> Table_name = new TextField<String>();
      Table_name.setEnabled(false);
      Table_name.setValue("Table name:");
-     Table_name.setReadOnly(true);
+     Table_name.setReadOnly(true);  */
 
 
-     /*      BUTTON CONNECT TO DATABASE        */
+     /*      BUTTON CONNECT TO DATABASE        
      Button buttonConnect = new Button("Connect");
      buttonConnect.addListener(Events.OnClick, new Listener<ButtonEvent>() {
        public void handleEvent(ButtonEvent be) {
@@ -392,7 +328,9 @@ public class GWTServiceGUI extends LayoutContainer {
          db_name.setValue("name @ localhost");
        }
      });
-    
+    */
+    p1.add(tree , new RowData(0.2, 1, new Margins(4)));
+     /*
      p1.add(database_type , new RowData(0.09, 1, new Margins(4)));
      p1.add(combo , new RowData(0.15, 1, new Margins(4)));
      p1.add(buttonConnect , new RowData(0.1, 1, new Margins(4)));
@@ -403,11 +341,11 @@ public class GWTServiceGUI extends LayoutContainer {
 
      p3.add(Table_name , new RowData(0.09, 1, new Margins(4)));
      p3.add(combo3 , new RowData(0.15, 1, new Margins(4)));
-
+*/
      simple.add(p1 , new RowData(1, -1, new Margins(4,4,4,14)));
-     simple.add(p2 , new RowData(1, -1, new Margins(4,4,4,14)));
+/*     simple.add(p2 , new RowData(1, -1, new Margins(4,4,4,14)));
      simple.add(p3 , new RowData(1, -1, new Margins(4,4,4,14)));
-
+*/
      //Label label1 = new Label("Database type:");
      
      //simple.add(connect , new RowData(0.1, -1, new Margins(4)));
@@ -421,7 +359,7 @@ public class GWTServiceGUI extends LayoutContainer {
      connect.addStyleName("pad-text");
      //connect.addText("Just a plain old tab");    
      panel.add(connect);
-/* *********************************************************************** */
+/* ******************* SELECT ATTRIBUTES ********************************* */
      TabItem selectAttributes = new TabItem("Select Attributes");
     // iconTab.setIcon(Resources.ICONS.table());
      selectAttributes.addStyleName("pad-text");
@@ -451,15 +389,13 @@ public class GWTServiceGUI extends LayoutContainer {
      return panel;
     }
 
-    class Stock extends BaseModel {
+    public static GWTServiceAsync getService() {
+        // Create the client proxy. Note that although you are creating the
+        // service interface proper, you cast the result to the asynchronous
+        // version of the interface. The cast is always safe because the
+        // generated proxy implements the asynchronous interface automatically.
 
-      public Stock() {
-      }
-
-      public Stock(String name) {
-        set("name", name);
-      }
-
+        return GWT.create(GWTService.class);
     }
 }
 
