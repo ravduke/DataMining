daf8e65ee490dade1d47f81b62c39e2581a94e67
Adrian Matoga <epi@atari8.info>
2011-05-15 23:05:16 +0200

    [server] timeless truth added.

--- GWTGUI/src/main/java/pl/edu/agh/ftj/datamining/server/GWTServiceImpl.java
+++ GWTGUI/src/main/java/pl/edu/agh/ftj/datamining/server/GWTServiceImpl.java
@@ -22,12 +22,13 @@ public class GWTServiceImpl extends RemoteServiceServlet implements GWTService {
     private static final long serialVersionUID = -15020842597334403L;
     private static List quotes = new ArrayList();
 
-    static { 
+    static {
         quotes.add("No great thing is created suddenly - Epictetus");
         quotes.add("Well done is better than well said - Ben Franklin");
         quotes.add("No wind favors he who has no destined port - Montaigne");
         quotes.add("Sometimes even to live is an act of courage - Seneca");
         quotes.add("Know thyself - Socrates");
+        quotes.add("Albo w javie rowy kopiesz, albo ona rowa kopie tobie - Abraham Lincoln");
     }
 
     public String myMethod(String s) {
