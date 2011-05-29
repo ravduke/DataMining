83327267c0fc1128b0887cc705aa9c9dd4cd374d
accent <jan.kosmala@gmail.com>
2011-05-29 12:09:03 +0200

    javadoc update

---
+++ build/generated-sources/jax-ws/pl/edu/agh/ftj/datamining/dbapi/webservice/GetDataSourcesResponse.java
@@ -0,0 +1,72 @@
+
+package pl.edu.agh.ftj.datamining.dbapi.webservice;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import pl.edu.agh.ftj.datamining.dbapi.core.xsd.BasicConfigurationDataSourceModel;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="return" type="{http://core.dbapi.datamining.ftj.agh.edu.pl/xsd}BasicConfigurationDataSourceModel" maxOccurs="unbounded" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "_return"
+})
+@XmlRootElement(name = "getDataSourcesResponse")
+public class GetDataSourcesResponse {
+
+    @XmlElement(name = "return", nillable = true)
+    protected List<BasicConfigurationDataSourceModel> _return;
+
+    /**
+     * Gets the value of the return property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the return property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getReturn().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link BasicConfigurationDataSourceModel }
+     * 
+     * 
+     */
+    public List<BasicConfigurationDataSourceModel> getReturn() {
+        if (_return == null) {
+            _return = new ArrayList<BasicConfigurationDataSourceModel>();
+        }
+        return this._return;
+    }
+
+}
--- test/pl/edu/agh/ftj/datamining/dbapi/webservice/WSIntegrationTest.java
+++ test/pl/edu/agh/ftj/datamining/dbapi/webservice/WSIntegrationTest.java
@@ -5,35 +5,66 @@
 
 package pl.edu.agh.ftj.datamining.dbapi.webservice;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import javax.xml.ws.WebServiceRef;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import pl.edu.agh.ftj.datamining.dbapi.webservice.*;
 
 /**
- *
+ * Testy integracyjne dla WebService'u
  * @author janek
+ * @version 1.0.0
  */
 public class WSIntegrationTest {
+
+    //@WebServiceRef(wsdlLocation = "C:\\Users\\janek\\Documents\\NetBeansProjects\\WekaDBApi\\WekaDBApi\\src\\META-INF\\wsdl\\localhost_8080\\axis2\\services\\DataAccessService.wsdl")
+    private DataAccessService client;
+
 
     public WSIntegrationTest() {
 
     }
 
-    @BeforeClass
-    public static void setUpClass() throws Exception {
+    @Test
+    /**
+     * Metoda sprawdzajaca zwracana liste zrodel danych
+     */
+    public void testDataSourceList(){
+        
+
+        try{
+        client = new DataAccessService();//new URL("http://localhost:8080/axis2/services/DataAccessService?wsdl"));
+        DataAccessServicePortType dataClient = client.getDataAccessServiceHttpSoap11Endpoint();
+        List<pl.edu.agh.ftj.datamining.dbapi.core.xsd.BasicConfigurationDataSourceModel> response = dataClient.getDataSources().getReturn();
+        assertEquals(response.size(),3);
+        }catch(Exception ex){
+            System.out.print(ex.getMessage());
+            ex.printStackTrace();
+            fail();
+        }
     }
 
-    @AfterClass
-    public static void tearDownClass() throws Exception {
+    /**
+     * Spradza czy przekazywane dane sa poprawne
+     */
+    @Test
+    public void testDataSource(){
+        try{
+        //client = new DataAccessService(new URL("http://localhost:8080/axis2/services/DataAccessService?wsdl"));
+        client = new DataAccessService();
+        DataAccessServicePortType dataClient = client.getDataAccessServiceHttpSoap11Endpoint();
+        String response = dataClient.getData("1", "iris.arff.csv");
+        assertEquals(response.length(), 1);
+        }catch(Exception ex){
+            System.out.print(ex.getMessage());
+            ex.printStackTrace();
+            fail();
+        }
     }
-
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
-
-
 
 }
